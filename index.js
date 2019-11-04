cdba55669c1d880f715325dab5810b2c02797105
raiden808 <raiden.nalasa@gmail.com>
2019-11-04 23:11:47 +0000

    change server

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
 })
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Mixing it up on port ${PORT}`)
 })
